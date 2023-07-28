0e6f3fe366c2da1affb7d545bbcf9a3ef6e5ad4e
toxirjon96 <tatu.toxirjon@gmail.com>
2023-07-28 10:54:36 +0500

    language controller added

--- lib/localization_library.dart
+++ lib/localization_library.dart
@@ -4,5 +4,4 @@ library localization;
 export 'dart:io';
 
 //model
-export 'package:localization/src/model/enum_category.dart';
 export 'package:localization/src/model/language_enum.dart';
--- lib/src/controller/lang_controller.dart
+++ lib/src/controller/lang_controller.dart
@@ -1,1 +1,47 @@
-class LanguageController {}
+import 'package:localization/localization_library.dart';
+
+class LanguageController {
+  final Language lang;
+  List<String> categories = [];
+
+  LanguageController(this.lang) {
+    switch (lang) {
+      case Language.en:
+        {
+          categories.addAll(
+            [
+              "Salads",
+              "Drinks",
+              "Fast Food",
+              "Diserts",
+              "National meals",
+            ],
+          );
+        }
+      case Language.ru:
+        {
+          categories.addAll(
+            [
+              "Салаты",
+              "Напитки",
+              "Быстрое питание",
+              "Десерты",
+              "Национальные блюда"
+            ],
+          );
+        }
+      case Language.uz:
+        {
+          categories.addAll(
+            [
+              "Salatlar",
+              "Ichimliklar",
+              "Fast food",
+              "Shirinliklar",
+              "NMilliy taomlar",
+            ],
+          );
+        }
+    }
+  }
+}
