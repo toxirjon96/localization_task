0daf22123e739e4563b3be5480b5c7585f700d01
toxirjon96 <tatu.toxirjon@gmail.com>
2023-07-28 11:06:01 +0500

    io repository added

--- lib/localization_library.dart
+++ lib/localization_library.dart
@@ -5,3 +5,9 @@ export 'dart:io';
 
 //model
 export 'package:localization/src/model/language_enum.dart';
+
+//repository
+export 'package:localization/src/repository/io_repository.dart';
+
+//contoller
+export 'package:localization/src/controller/lang_controller.dart';
---
+++ lib/src/repository/io_repository.dart
@@ -0,0 +1,9 @@
+abstract interface class IORepostory {
+  void write(String info);
+
+  void writeln(String info);
+
+  String read(String info);
+
+  int readInt(String info);
+}
